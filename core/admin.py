0113b394692e5f934af47febabf77d1f3d5f87ea
Sandy <sandyluimo@hotmail.com>
2022-07-02 21:58:03 -0300

    agenda um pouco mais trabalhada

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+
+class EventoAdmin (admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')  # o que quer que aparece dentro do django na parte de Core
+    list_filter = ('titulo', 'data_evento',) # sempre tem que ter a vírgula no final, do contrário dá erro
+
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Criando tabelas com models
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100) # esse campo tem no máximo 100 caracteres
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')  # verbose_name é para colocar o nome do jeito que
+    # quer que apareça no django
+    data_criacao = models.DateTimeField(auto_now=True, verbose_name='Data da Criação')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # para exigir que a tabela se chame evento (ou outro nome que quiser)
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
