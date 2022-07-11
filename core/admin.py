5b20f66ad6c95a2001faa5ad5d3167cc6dc83edb
Sandy <sandyluimo@hotmail.com>
2022-07-11 01:55:20 -0300

    Atribuindo mais algumas configurações na agenda

--- core/admin.py
+++ core/admin.py
@@ -4,7 +4,7 @@ from core.models import Evento
 # Register your models here.
 
 class EventoAdmin (admin.ModelAdmin):
-    list_display = ('titulo', 'data_evento', 'data_criacao')  # o que quer que aparece dentro do django na parte de Core
+    list_display = ('id', 'titulo', 'data_evento', 'data_criacao')  # o que quer que aparece dentro do django na parte de Core
     list_filter = ('titulo', 'data_evento',) # sempre tem que ter a vírgula no final, do contrário dá erro
 
 admin.site.register(Evento, EventoAdmin)
--- core/views.py
+++ core/views.py
@@ -33,6 +33,34 @@ def lista_eventos(request):
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
 
+@login_required(login_url='/login/')  # requerer o login para acessar essa parte
+def evento(request):
+    id_evento = request.GET.get('id')
+    dados = {}
+    if id_evento:
+        dados['evento'] = Evento.objects.get(id=id_evento)
+    return render(request, 'evento.html', dados)
 
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        id_evento = request.POST.get('id_evento')
+        if id_evento:
+            Evento.objects.filter(id=id_evento).update(titulo=titulo, data_evento=data_evento, descricao=descricao)
+        else:
+            Evento.objects.create(titulo=titulo, data_evento=data_evento, descricao=descricao, usuario=usuario)
+    return redirect('/')
+
+@login_required(login_url='/login/')
+def delete_evento(request, id_evento):
+    usuario = request.user
+    evento = Evento.objects.get(id=id_evento)
+    if usuario == evento.usuario:
+        evento.delete()
+    return redirect('/')
 
 
